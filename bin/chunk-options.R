21b9fdd224a4eade9968172e5704bfb702227fdf
Sarah Stevens <sarah.stevens@wisc.edu>
2020-07-09 09:33:20 -0500

    Take two on fixing the fig paths

--- bin/chunk-options.R
+++ bin/chunk-options.R
@@ -29,7 +29,7 @@ knitr_fig_path <- function(prefix) {
 ## `fig.process` option.
 
 opts_chunk$set(tidy = FALSE, results = "markup", comment = NA,
-               fig.align = "center", fig.path = "fig/rmd-",
+               fig.align = "center", fig.path = "_episodes/fig/rmd-",
                fig.process = fix_fig_path,
                fig.width = 8.5, fig.height = 8.5,
                fig.retina = 2)
